f97d18f31b61be2a05210a17b98784dc8790a344
Yehuda Hason <yehudaxt@gmail.com>
2026-03-30 15:08:20 +0300

    not found4

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const Home = () => {
     }
 
     setLoading(true); // ✅ start loading
-
+    setShowForcast(false); // ✅ hide forecast while searching
     try {
       const results = await searchCities(value);
       setCities(results);
