ceaef1ebdad1f8ebc0d35b01abaa2ab05dd3cfda
Yehuda Hason <yehudaxt@gmail.com>
2026-03-30 15:56:53 +0300

    not found5

--- app/api/cities/route.ts
+++ app/api/cities/route.ts
@@ -3,7 +3,16 @@ import { NextResponse } from "next/server";
 import { City } from "@/app/types/types";
 
 // normalize query same way as your dataset
-const normalize = (str: string) => str.toLowerCase().replace(/\s/g, "");
+const normalize = (str: string) => {
+  str = toEnglish(str);
+  return str.toLowerCase().replace(/\s/g, "");
+};
+
+function toEnglish(text: string): string {
+  return text
+    .normalize("NFD") // split letters + accents
+    .replace(/[\u0300-\u036f]/g, ""); // remove accents
+}
 
 const indexedCities: City[] = cities as City[];
 
